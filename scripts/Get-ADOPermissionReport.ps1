b418281ca5015978768dc6200dcce3af92ad78b3
Richard Phillips <38532979+xXfinchXx@users.noreply.github.com>
2021-03-10 14:33:09 -0600

    Updates for logging and mistakes I made

--- scripts/Get-ADOPermissionReport.ps1
+++ scripts/Get-ADOPermissionReport.ps1
@@ -2,8 +2,8 @@ param(
     [string]$ADOAccount = "",
     [string]$ADOprojectName = "",
     [string]$ADOpat = "",
-    [string]$searchRepoName = '',#Ability to narrow to specific list or regex. Update Line 21 for list with -in
-    $RootPath=''#Root path to where report folders/files go
+    [string]$searchRepoName = '',#Ability to narrow to specific list or regex. Update Line 20 for list with -in
+    [string]$RootPath = ''#Root path to where report folders/files go
 )
 #Set Creds as Global Vars to easily Reuse within this session
 $Global:ADOAccount = $ADOAccount
@@ -17,17 +17,17 @@ $uri = "https://dev.azure.com/$($ADOAccount)/$($ADOprojectName)/_apis/git/reposi
 $result = Invoke-RestMethod -Uri $uri -Method Get -ContentType "application/json" -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
 #Checking if SearchRepoName was set in the params
 if ($searchRepoName){
-    $out=$result.value | Where name -Match "$($SearchRepoName)" 
+    $repolist=$result.value | Where name -Match "$($SearchRepoName)" 
 }else{
-    $out = $result.value
+    $repolist = $result.value
 }
-return $out
 #URL for Permission Report Creation
 $uri="https://dev.azure.com/${ADOAccount}/_apis/permissionsreport?api-version=6.0-preview.1"
 #Need a tag to differentiate between each report of the same repo
 $date = Get-Date -Format "yyyyMMddhhmm"
 
 Foreach ($repo in $repolist){
+    Write-host "Currently Working on $($Repo.name)"
     #Create a Json object for create action and for reuse in script
     $json = [pscustomobject]@{
         descriptors=@() 
@@ -46,15 +46,18 @@ Foreach ($repo in $repolist){
     #set GetReport to null so that the while loop works
     $GETReport=$null
     #Checking to see if the report is ready or not
+    Write-Host "Waiting for report to return status: completedSuccessfully"
     while ($GETReport.reportStatus -notmatch "completedSuccessfully"){
         Start-Sleep -Seconds 5
         $GETReport= (invoke-restmethod -method Get -uri "https://dev.azure.com/${ADOAccount}/_apis/permissionsreport?api-version=6.0-preview.1" -ContentType "application/json" -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} ).value| Where {$_.reportname -Match $json.reportname}
     }
     #Report is ready for Download at this point so the Var Final is storing that report
+    Write-Host "Report is ready for download. Starting Download now."
     $final =invoke-restmethod -method Get -uri "https://dev.azure.com/${ADOAccount}/_apis/permissionsreport/$($GetReport.id)/download?api-version=6.0-preview.1" -ContentType "application/json" -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
     #remove characters that shouldn't be there
     $Identities = ($final -replace 'ï»¿','') | ConvertFrom-Json
     #making the report to look like the current output
+    Write-host "Converting Report to correct format"
     $Report=[pscustomObject]@{
         Permissions=@(
             @{Path = "$($ADOProjectName)/$($Repo.name)"}
@@ -69,7 +72,8 @@ Foreach ($repo in $repolist){
         )
     }
     #Test to see if path exists and creates if it doesn't
-    if (!(Test-Path "$($RootPath)\$($repo.name)")){mkdir "$($RootPath)\$($repo.name)"}
+    if (!(Test-Path "$($RootPath)\$($repo.name)")){mkdir "$($RootPath)\$($repo.name)";Write-Host "Directory Created for $($repo.name)"}
     #Convert report back to Json and outputs to Location with folders
+    Write-Host "Saving report for $($repo.name) at $($RootPath)\$($repo.name)\$($json.reportname).json"
     $Report | ConvertTo-Json -Depth 10 | Out-File "$($RootPath)\$($repo.name)\$($json.reportname).json"
 }
