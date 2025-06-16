30eb31e01be8082152cc918d0981f82afa38a0df
Richard Phillips <38532979+xXfinchXx@users.noreply.github.com>
2025-06-16 07:57:10 -0500

    Create Get-ADORepoBranches.ps1

---
+++ public/Get-ADORepoBranches.ps1
@@ -0,0 +1,23 @@
+function Get-ADORepoBranches {
+    Param(
+        [Parameter(Mandatory)][string]$repositoryId,
+        $ADOprojectName
+    )
+    begin {
+        if (!($ADOpat)) {
+            Write-Warning "Looks like you haven't set your connection yet. Let me help you with that."
+            $projectName = Read-Host -Prompt 'What is your Azure DevOps Project Name?'
+            $vstsAccount = Read-host -Prompt 'What is your Azure DevOps Account Name?'
+            $PAT = Read-Host -Prompt 'What is your Azure DevOps PAT (Personal Access Token)?'
+            Set-ADOconnection -ADOAccount $vstsAccount -ADOprojectName $projectName -ADOpat $PAT
+        }
+        $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $ADOUser,$ADOpat)))
+    }
+    process {
+        $uri = "https://dev.azure.com/$($ADOAccount)/$($ADOprojectName)/_apis/git/repositories/$($repositoryId)/refs?api-version=7.1"
+        $result = Invoke-RestMethod -Uri $uri -Method Get -ContentType "application/json" -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)}
+    }
+    end {
+        return $result
+    }
+}
